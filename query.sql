78777deded260010d78443aa9ca28ce5ddd845a2
Mark Athanason <mark.athanason@vumc.org>
2019-08-24 10:29:14 -0500

    v2

--- query.sql
+++ query.sql
@@ -1,7 +1,12 @@
-SELECT * FROM package
+SELECT * INTO package_bak FROM package
 INNER JOIN product
-ON package.product_id = product.product_id;
+ON package.product_id = product.product_id
+WHERE dea_schedule IS null
 
 UPDATE product
 SET dea_schedule = 'CVI'
-WHERE dea_schedule IS NULL;
+WHERE product_type_name LIKE '%PRESCRIPTION%' AND dea_schedule IS NULL;
+
+UPDATE product
+SET dea_schedule = 'OTC'
+WHERE product_type_name LIKE '%OTC%' AND dea_schedule IS NULL;
